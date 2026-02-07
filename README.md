2e4f3d476ee49e2f3608faeccd6104eccccf6c46
enzogran01 <yournarrator013>
2026-02-07 16:56:34 -0300

    :tada: Commit inicial

---
+++ README.md
@@ -0,0 +1,54 @@
+# Minecoords
+
+![GitHub repo size](https://img.shields.io/github/repo-size/enzogran01/minecoords?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/enzogran01/minecoords?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/enzogran01/minecoords?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/enzogran01/minecoords?style=for-the-badge)
+![Bitbucket open pull requests](https://img.shields.io/bitbucket/pr-raw/enzogran01/minecoords?style=for-the-badge)
+
+<img src="https://i.pinimg.com/originals/ab/be/8f/abbe8fde3c7ab201c38e66b03b163e2c.gif" alt="Landscape">
+
+> Lightweight single-page web app to save, display, and convert Minecraft coordinates between Overworld and Nether.
+
+### Improvements and upcoming features
+
+The project is still under development, and the next updates will focus on the following tasks:
+
+- [x] Add new coordinates  
+- [ ] Delete/Edit coordinates  
+- [ ] Add worlds  
+- [ ] Login system  
+- [ ] World sharing  
+
+## 📫 Contributing to Minecoords
+
+To contribute to Minecoords, follow these steps:
+
+1. Fork this repository.  
+2. Create a branch: `git checkout -b <branch_name>`.  
+3. Make your changes and commit them: `git commit -m '<commit_message>'`  
+4. Push to the original branch: `git push origin <project_name> / <location>`  
+5. Create a pull request.  
+
+Alternatively, check the GitHub documentation on [how to create a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 🤝 Contributors
+
+We thank the following people who contributed to this project:
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/enzogran01" title="enzogran01 GitHub profile">
+        <img src="https://avatars3.githubusercontent.com/u/168101127" width="100px;" alt="enzogran01 GitHub avatar"/><br>
+        <sub>
+          <b>enzogran01</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+## 📝 License
+
+This project is licensed. See the [LICENSE](LICENSE.md) file for more details.
--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ class Coord {
         this.x = x;
         this.z = z;
         this.dimension = dimension;
+        this.bgColor = dimension === "ove" ? "bg-blue-400" : dimension === "net" ? "bg-red-400" : "bg-purple-400";
     }
 
     toDimension() {
@@ -32,6 +33,7 @@ const saveCoordBtn = document.getElementById("saveCoordBtn");
 const coordsDisplayDiv = document.getElementById("coordsDisplayDiv");
 const newCoordBtn = document.getElementById("newCoordBtn");
 const newCoordDialog = document.getElementById("newCoordDialog");
+const closeDialogBtn = document.getElementById("closeDialog");
 
 // functions
 function clearInputs () {
@@ -42,7 +44,7 @@ function clearInputs () {
 }
 
 function formatCoordCard (coord) {
-    const { name, x, z } = coord;
+    const { name, x, z, bgColor } = coord;
 
     const thisDimension = coord.formatDimension();
     const card = document.createElement("div");
@@ -50,7 +52,7 @@ function formatCoordCard (coord) {
     const pX = document.createElement("p");
     const pZ = document.createElement("p");
     const pDimension = document.createElement("p");
-    
+
     h3Name.textContent = `${name}`;
     pX.textContent = `X: ${x}`;
     pZ.textContent = `Z: ${z}`;
@@ -61,18 +63,22 @@ function formatCoordCard (coord) {
     card.appendChild(pZ);
     card.appendChild(pDimension);
 
-    card.classList.add("flex", "flex-col", "bg-blue-500", "text-2xl", "p-1");
+    h3Name.classList.add("text-3xl", "mb-4");  
+    card.classList.add("flex", "flex-col", "items-center", bgColor, "text-2xl", "p-2", "h-fit");
 
     if (thisDimension !== "The End") {
         const btnDimension = document.createElement("button");
         const spanAlternateDimension = document.createElement("p");
+
+        btnDimension.classList.add("p-2", "rounded-md", "bg-slate-700", "text-white", "mt-4", "mb-2", "transition-all", "duration-200", "cursor-pointer", "active:bg-slate-500", "active:-translate-y-1");
+        spanAlternateDimension.classList.add("text-center", "text-xl");
 
         btnDimension.textContent = `Converter para ${thisDimension === "Overworld" ? "Nether" : "Overworld"}`;
         card.appendChild(btnDimension);
 
         btnDimension.addEventListener("click", () => {
             const converted = coord.toDimension();
-            spanAlternateDimension.textContent = `(${(thisDimension === "Overworld" ? "Nether" : "Overworld").toUpperCase()}) X: ${converted.x.toFixed(1)} / Z: ${converted.z.toFixed(1)}`;
+            spanAlternateDimension.textContent = `(${(thisDimension === "Overworld" ? "Nether" : "Overworld").toUpperCase()}) X: ${converted.x.toFixed(0)} | Z: ${converted.z.toFixed(0)}`;
             card.appendChild(spanAlternateDimension);
         });
     }
@@ -86,7 +92,6 @@ function renderCards() {
     allCoords.forEach(coord => {
         const card = formatCoordCard(coord);
         coordsDisplayDiv.appendChild(card);
-        console.log(card);
     });
 }
 
@@ -98,20 +103,34 @@ function toggleDialog() {
     newCoordDialog.showModal();
 }
 
+function validaFormulario() {
+    if (coordName.value.trim() === "") {
+        return false;
+    }
+    if (xCoord.value.trim() === "" || zCoord.value.trim() === "") {
+        return false;
+    }
+    return true;
+}
+
 // eventlisteners
 newCoordBtn.addEventListener("click", toggleDialog);
+closeDialogBtn.addEventListener("click", () => {toggleDialog(), clearInputs()});
 
 saveCoordBtn.addEventListener("click", () => {
-    toggleDialog();
+    if (!validaFormulario()) {
+        return;
+    }
 
     const coord = new Coord(
-        coordName.value,
-        Number(xCoord.value),
-        Number(zCoord.value),
+        coordName.value.trim(),
+        Number(xCoord.value.trim()),
+        Number(zCoord.value.trim()),
         dimension.value
     );
-
+    
     allCoords.push(coord);
     clearInputs();
+    toggleDialog();
     renderCards();
 });
